31efd75bb4cf8b0f8808ff922e3e5c7beadd3f73
Haidang124 <haidangluu94@gmail.com>
2020-10-15 02:27:02 +0700

    api signup

--- src/app/controllers/userController.js
+++ src/app/controllers/userController.js
@@ -1,3 +1,59 @@
+const User = require("../../model/userModel");
+const jwt = require("jsonwebtoken");
+const maxAge = 3 * 24 * 60 * 60;
+const createToken = (id) => {
+  return jwt.sign({ id }, "net ninja secret", {
+    expiresIn: maxAge,
+  });
+};
+const handleErrors = (err) => {
+  console.log(err.message, err.code);
+  let errors = { email: "", password: "" };
+
+  // incorrect email
+  if (err.message === "incorrect email") {
+    errors.email = "That email is not registered";
+  }
+
+  // incorrect password
+  if (err.message === "incorrect password") {
+    errors.password = "That password is incorrect";
+  }
+
+  // duplicate email error
+  if (err.code === 11000) {
+    errors.email = "that email is already registered";
+    return errors;
+  }
+
+  // validation errors
+  if (err.message.includes("user validation failed")) {
+    // console.log(err);
+    Object.values(err.errors).forEach(({ properties }) => {
+      // console.log(val);
+      // console.log(properties);
+      errors[properties.path] = properties.message;
+    });
+  }
+
+  return errors;
+};
 module.exports.getUser = (req, res, next) => {
-    res.json({"foo": "bar"});
+  res.json({ foo: "bar" });
+};
+module.exports.loginUser = (req, res, next) => {
+  res.json({ foo: "bar" });
+};
+module.exports.signUp = async (req, res) => {
+  const { email, password } = req.body;
+  console.log(email);
+  try {
+    const user = await User.create({ email, password });
+    const token = createToken(user._id);
+    res.cookie("jwt", token, { httpOnly: true, maxAge: maxAge * 1000 });
+    res.status(201).json({ user: user._id });
+  } catch (err) {
+    const errors = handleErrors(err);
+    res.status(400).json({ errors });
+  }
 };
