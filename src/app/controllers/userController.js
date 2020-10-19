d429b0c46a29192e73b4b246b589ccbafc72749a
Haidang124 <haidangluu94@gmail.com>
2020-10-20 02:52:29 +0700

    api login , signup

--- src/app/controllers/userController.js
+++ src/app/controllers/userController.js
@@ -1,59 +1,43 @@
 const User = require("../../model/userModel");
 const jwt = require("jsonwebtoken");
+const {
+  handleErrorResponse,
+  handleSuccessResponse,
+} = require("../../helper/responseHelper");
 const maxAge = 3 * 24 * 60 * 60;
 const createToken = (id) => {
-  return jwt.sign({ id }, "net ninja secret", {
+  return jwt.sign({ id }, "ptud web", {
     expiresIn: maxAge,
   });
 };
-const handleErrors = (err) => {
-  console.log(err.message, err.code);
-  let errors = { email: "", password: "" };
-
-  // incorrect email
-  if (err.message === "incorrect email") {
-    errors.email = "That email is not registered";
+module.exports.getUser = (req, res, next) => {};
+module.exports.loginUser = async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    const user = await User.login(email, password);
+    const token = createToken(user._id);
+    res.cookie("token", token, { httpOnly: true, maxAge: maxAge * 1000 });
+    return handleSuccessResponse(
+      res,
+      200,
+      { user: user.id },
+      "Đăng nhập thành công !"
+    );
+  } catch (error) {
+    return handleErrorResponse(res, 400, "Đăng nhập thất bại !");
   }
-
-  // incorrect password
-  if (err.message === "incorrect password") {
-    errors.password = "That password is incorrect";
-  }
-
-  // duplicate email error
-  if (err.code === 11000) {
-    errors.email = "that email is already registered";
-    return errors;
-  }
-
-  // validation errors
-  if (err.message.includes("user validation failed")) {
-    // console.log(err);
-    Object.values(err.errors).forEach(({ properties }) => {
-      // console.log(val);
-      // console.log(properties);
-      errors[properties.path] = properties.message;
-    });
-  }
-
-  return errors;
-};
-module.exports.getUser = (req, res, next) => {
-  res.json({ foo: "bar" });
-};
-module.exports.loginUser = (req, res, next) => {
-  res.json({ foo: "bar" });
 };
 module.exports.signUp = async (req, res) => {
   const { email, password } = req.body;
-  console.log(email);
   try {
     const user = await User.create({ email, password });
-    const token = createToken(user._id);
-    res.cookie("jwt", token, { httpOnly: true, maxAge: maxAge * 1000 });
-    res.status(201).json({ user: user._id });
+    return handleSuccessResponse(
+      res,
+      200,
+      { user: user.id },
+      "Đăng kí thành công !"
+    );
   } catch (err) {
-    const errors = handleErrors(err);
-    res.status(400).json({ errors });
+    return handleErrorResponse(res, 400, "Đăng ki thất bại !");
   }
 };
